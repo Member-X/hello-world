26bca4ba5272ad0963bf96fdaffce90312b77924
'Steve <sowens12@DTEC427-R3-02.academic.hccfl.edu>
2017-01-26 20:05:28 -0500

    This is a sample from class

---
+++ AddNumbers/src/addnumbers/AddNumbers.java
@@ -0,0 +1,17 @@
+package addnumbers;
+
+/**
+ *
+ * @author sowens12
+ */
+public class AddNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Adding 1 + 1 equals 2");
+    }
+    
+}
